8b3a6e8f1d5b1732521abc3665844c0fa43f64db
Jefferson Augusto Ribeiro <jeffersonari@yahoo.com.br>
2022-03-27 17:23:02 -0300

    Initial commit

---
+++ src/main.kt
@@ -0,0 +1,14 @@
+fun main(){
+   println("Bem vindo ao Bytebank")
+   //Val criar varialvel que não pode ser alterada
+    // é obrigatorio inicializar a varialvel
+    val titular = "Jefferson"
+    val numeroConta = 19923
+    var saldo = 0.0
+    saldo = 100.0 + 2.0
+    saldo += 200
+
+    println("Titular: $titular")
+    println("Numero da Conta: $numeroConta")
+    println("Saldo: $saldo")
+}
